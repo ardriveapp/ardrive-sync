c20760f1e7f37e2359fa220cb7344786da589b52
unknown <vilenarios@gmail.com>
2022-02-20 11:14:07 -0500

    Adds retries for getData

--- src/gateway.ts
+++ src/gateway.ts
@@ -1,15 +1,30 @@
 import { arweave } from './arweave';
+import axios from 'axios';
+import axiosRetry, { exponentialDelay } from 'axios-retry';
 
 // Gets only the data of a given ArDrive Data transaction (U8IntArray)
-export async function getTransactionData(txid: string): Promise<string | Uint8Array> {
-	try {
-		const data = await arweave.transactions.getData(txid, { decode: true });
-		return data;
-	} catch (err) {
-		console.log('Error getting transaction data for Txid %s', txid);
-		console.log(err);
-		return Promise.reject(err);
-	}
+export async function getTransactionData(txId: string): Promise<string | Uint8Array> {
+	const protocol = 'https';
+	const host = 'arweave.net';
+	const portStr = '';
+	const reqURL = `${protocol}://${host}${portStr}/${txId}`;
+	const axiosInstance = axios.create();
+	const maxRetries = 5;
+	axiosRetry(axiosInstance, {
+		retries: maxRetries,
+		retryDelay: (retryNumber) => {
+			console.error(`Retry attempt ${retryNumber}/${maxRetries} of request to ${reqURL}`);
+			return exponentialDelay(retryNumber);
+		}
+	});
+	const {
+		data: txData
+	}: {
+		data: Buffer;
+	} = await axiosInstance.get(reqURL, {
+		responseType: 'arraybuffer'
+	});
+	return txData;
 }
 
 // Get the latest status of a transaction
